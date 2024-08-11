152e4fe85f9b5995209e14006e13f1c047d2de52
Reagan Henke <reaganhenke@gmail.com>
2024-08-11 17:38:30 -0400

    add search filter

--- README.md
+++ README.md
@@ -13,10 +13,11 @@ version 17.0.5.
 - [x] fix styling of the sort
 - [x] add "skip to ingredients" button
 - [x] style checkboxes
-- [ ] Add text filter to search for items
+- [x] Add text filter to search for items
 - [ ] fix sort for seasons
 - [ ] add version control to the key thats saved
 - [ ] maybe add type of recipe source, so you can sort by that? even if its not displayed?
+- [ ] Add a reset (clear all checks)
 - [ ] Update this README
 
 stretch goals
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,11 @@ export interface MiniIng {
   c: number;
 }
 
+export interface FilterObj {
+  hideCooked: boolean;
+  substring: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -34,36 +39,50 @@ export class AppComponent {
   ingredientDataSource = new MatTableDataSource(this.generateIngredients());
   hideUsedIngredients = new FormControl(false);
   categoryType = categoryType;
+  currentFilter: FilterObj = {
+    hideCooked: false,
+    substring: ''
+  };
+  ingNameSearch = new FormControl('');
   @ViewChild('recipeSort', { static: false }) recipeSort!: MatSort;
   @ViewChild('ingredientSort', { static: false }) ingredientSort!: MatSort;
 
-  constructor(private cdRef: ChangeDetectorRef){
+  constructor(private cdRef: ChangeDetectorRef) {
     const loadedData = this.loadFromBrowser();
     if (loadedData) {
       const parsedJson = JSON.parse(loadedData);
-      this.hideUsedIngredients.setValue(parsedJson.hide);
 
-      parsedJson.recipes.forEach((rec : MiniRecipe, ind: number) => {
+      parsedJson.recipes.forEach((rec: MiniRecipe, ind: number) => {
         ALLRECIPES[ind].cooked = !!rec.c;
         ALLRECIPES[ind].learned = !!rec.l;
       })
 
       const newIngs = this.generateIngredients();
-      parsedJson.ingredients.forEach((ing : MiniIng, ind: number) => {
+      parsedJson.ingredients.forEach((ing: MiniIng, ind: number) => {
         newIngs[ind].collected = !!ing.c;
       })
 
       this.recipeDataSource = new MatTableDataSource(ALLRECIPES);
       this.ingredientDataSource = new MatTableDataSource(newIngs);
+      this.hideUsedIngredients.setValue(parsedJson.hide);
+      this.currentFilter.hideCooked = parsedJson.hide;
     }
   }
 
   ngAfterViewInit() {
     this.recipeDataSource.sort = this.recipeSort;
     this.ingredientDataSource.sort = this.ingredientSort;
-    this.ingredientDataSource.filterPredicate = 
-      (ing: Ingredient, filter: string) => {
-        return (filter=="true") ? !ing.recipeCooked : true};
+
+    this.ingredientDataSource.filterPredicate =
+      ((ing: Ingredient, filter: string) => {
+        const filterObject = JSON.parse(filter) as FilterObj;
+        const filterA = (filterObject.hideCooked == true) ? !ing.recipeCooked : true;
+        const filterB = filterObject.substring ? ing.name.toLowerCase().includes(filterObject.substring.trim()) : true;
+        return filterA && filterB;
+      });
+
+    this.ingredientDataSource.filter = JSON.stringify(this.currentFilter);
+
     this.cdRef.detectChanges()
   }
 
@@ -93,7 +112,8 @@ export class AppComponent {
       x.collected = event.checked;
     });
 
-    this.ingredientDataSource.filter = this.hideUsedIngredients?.value?.toString() || '';
+    this.currentFilter.hideCooked = event.checked;
+    this.ingredientDataSource.filter = JSON.stringify(this.currentFilter);
     this.saveToBrowser();
   }
 
@@ -103,7 +123,8 @@ export class AppComponent {
   }
 
   toggleHideUsedIngredients(event: MatCheckboxChange) {
-    this.ingredientDataSource.filter = event.checked.toString(); 
+    this.currentFilter.hideCooked = event.checked;
+    this.ingredientDataSource.filter = JSON.stringify(this.currentFilter);
     this.saveToBrowser();
   }
 
@@ -121,12 +142,18 @@ export class AppComponent {
 
     let dataObj = {
       hide: this.hideUsedIngredients.value,
-      recipes: this.recipeDataSource.data.map(r => { return {c: this.shorten(r.cooked), l: this.shorten(r.learned)}}),
-      ingredients: this.ingredientDataSource.data.map(i => { return {c: this.shorten(i.collected)}})
+      recipes: this.recipeDataSource.data.map(r => { return { c: this.shorten(r.cooked), l: this.shorten(r.learned) } }),
+      ingredients: this.ingredientDataSource.data.map(i => { return { c: this.shorten(i.collected) } })
     }
 
     // console.log("save: ", JSON.stringify(dataObj));
 
     localStorage.setItem('stardewcooking', JSON.stringify(dataObj));
+  }
+
+  handleSearch() {
+    this.currentFilter.substring = this.ingNameSearch.value?.trim() || '';
+    this.ingredientDataSource.filter = JSON.stringify(this.currentFilter);
+    this.cdRef.detectChanges()
   }
 }
